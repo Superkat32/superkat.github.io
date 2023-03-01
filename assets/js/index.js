d4b93e84985ee230b919077acd33e4b23bccf72c
Superkat32 <superkat1806@gmail.com>
2023-03-01 00:01:45 -0500

    Began work on updating the About Me page
    REFACTOR: Made the first paragraph in the About Me page a very short paragraph and a bullet point list thingy

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,35 +2,17 @@
 //This part isn't actually used, it's simply copy-pasted into the .md file.
 //Why? Because I couldn't figure out how to get it working otherwise, and I don't want to delete this.
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const visitedKey = "visited";
+var i = 0;
+var txt = 'About Me - My Coding Journey Thus Far...';
+var speed = 70;
 
-window.onload = () => {
-  const h1 = document.querySelector("h1");
-
-  if (!localStorage.getItem(visitedKey)) {
-    let iterations = 0;
-
-    const interval = setInterval(() => {
-      console.log("text is being hovered over!");
-      h1.innerText = h1.dataset.value
-        .split("")
-        .map((letter, index) => {
-          if (index < iterations) {
-            return h1.dataset.value[index];
-          }
-
-          return letters[Math.floor(Math.random() * 26)];
-        })
-        .join("");
-
-      if (iterations >= h1.dataset.value.length) clearInterval(interval);
-
-      iterations += 1 / 3;
-    }, 30);
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    if(i == 10) {
+      speed = 90;
+    }
+    setTimeout(typeWriter, speed);
   }
-};
-
-window.addEventListener("beforeunload", () => {
-  localStorage.removeItem(visitedKey);
-});
+}
